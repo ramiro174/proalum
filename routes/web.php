7bf82c6a061a8ff9adaf75a5f4d78bde7e8838fa
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-03-09 09:18:14 -0600

    ad cambios

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,19 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+    
+    use Illuminate\Routing\Route;
+    
+    Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+    
+    
+    Route::get("/who", function () {
+        
+        return exec('whoami');
+    });
