479e8d4caa881866c6513a1ebe7840f84815e6b0
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-03-09 00:20:29 -0600

    ad cambios
    (cherry picked from commit 522ce621bbf8a55c76a787b938092f0b6ac03aa3)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,7 +68,8 @@
     <body>
     <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
+        
+            <div class="top-right links">
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
                     @else
