49aee41499c689482f2ba3dd5fe4d17261b8178d
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-03-09 00:18:37 -0600

    ad cambios
    (cherry picked from commit 522ce621bbf8a55c76a787b938092f0b6ac03aa3)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,7 +66,7 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+    <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
