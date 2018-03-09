53c6fbb2cbb14f8fa29f3a5102273465789b5679
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-03-09 00:00:24 -0600

    ad cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Listo para empezaraaa!!aa
+                   Listo para empezaraaa!!aaweew
                 </div>
 
                 <div class="links">
