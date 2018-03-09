11fd7fbbae8a10269c14dc007c5b66ae673baa47
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-03-09 09:21:24 -0600

    ad cambios

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@
 |
 */
     
-    
     Route::get('/', function () {
     return view('welcome');
 });
@@ -20,8 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
     
-    
-    Route::get("/who", function () {
-        
-        return exec('whoami');
+    Route::get('/who', function () {
+        return "ss";
     });
