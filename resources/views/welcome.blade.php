ac69f6151d3d1a259c4d87fba046abb1fa5a61eb
ramiro174 <ramiro174@gmail.com>
2018-03-08 23:32:10 -0600

    ad cambios

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Listo para empezar!!
                 </div>
 
                 <div class="links">
