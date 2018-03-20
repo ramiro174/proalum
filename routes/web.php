c3e1168bf38aa245d1154b6e7ec93da3f72bc4d2
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-03-20 08:05:43 -0600

    recuperar valor

--- routes/web.php
+++ routes/web.php
@@ -1,52 +1,50 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
     
     use App\models\Resultado;
     use Illuminate\Http\Request;
     
     Route::get('/', function () {
-    return view('welcome');
-});
+        return view('welcome');
+    });
     
     Route::get('/baraja', function () {
         
-       
-        return  array("numero"=>rand(1,13));
+        
+        return array("numero" => rand(1, 13));
     });
     Route::post('/enviar', function (Request $resq) {
-        $r=new Resultado();
-        
-        $numero=$resq->input("numero");
-        $nombre=$resq->input("nombre");
-        
-        $r->numero=$numero;
-        $r->nombre=$nombre;
+        $r = new Resultado();
+        $numero = $resq->input("numero");
+        $nombre = $resq->input("nombre");
+        $r->numero = $numero;
+        $r->nombre = $nombre;
         $r->save();
-        
-        
         return $r;
-        
-    
     });
     
-
-Auth::routes();
-
-
-
-
-
-Route::get('/home', 'HomeController@index')->name('home');
+    
+    Route::get("/resultadosfinales",function(){
+       
+         view("resultadosvista");
+        
+        
+    });
+    
+    Auth::routes();
+    
+    
+    Route::get('/home', 'HomeController@index')->name('home');
     
     Route::get('/who', function () {
         return exec('whoami');
