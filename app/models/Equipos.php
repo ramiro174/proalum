845f07bddb77d2dc08eebfbcd36f9dee6ea33dd1
Ramiro Esquivel Duran <ramiro174@gmail.com>
2018-06-27 13:51:24 -0500

    fix equipos

--- app/models/Equipos.php
+++ app/models/Equipos.php
@@ -1,45 +1,40 @@
 <?php
-
-namespace App\models;
-
-use Illuminate\Database\Eloquent\Model;
-use Auth;
-class Equipos extends Model
-{
-    protected $table='equipos';
-    protected $primaryKey='id';
-    protected $fillable=["nombreequipo"];
-
-    public function userLider(){
-    	return $this->belongsToMany('App\models\User',"equipo_user","equipo_id","user_lider_id")
-    	->withPivot('user_id_lider')->withTimestamps();
-    }
-
-    public function userMiembro(){
-    	return $this->belongsToMany('App\models\User',"equipo_user","equipo_id","user_id")
-    	->withPivot('user_id')->withTimestamps();
-    }
-    public function proyecto()
-    {
-        return $this->hasMany('App\models\Proyectos','equipos_id');
-    }
-
-    public  function scopeMisequipos($query)
-    {
     
-        $usuario = Auth::user()->id;
-        return $query->whereHas('userMiembro', function($q) use ($usuario){
-            $q->where('user_id',$usuario);
-        })->get();
+    namespace App\models;
+    
+    use Illuminate\Database\Eloquent\Model;
+    use Auth;
+    
+    class Equipos extends Model
+    {
+        protected $table = 'equipos';
+        protected $primaryKey = 'id';
+        protected $fillable = ["nombreequipo"];
         
         
-//        $usuario = Auth::user()->id;
-//        $equipos = Equipos::whereHas('userMiembro', function($q) use ($usuario){
-//            $q->where('user_id',$usuario);
-//        })->get();
-//        return $equipos;
+        public function userLider()
+        {
+            return $this->belongsToMany('App\models\User', "equipo_user", "equipo_id", "user_lider_id")
+                ->withPivot('user_id_lider')->withTimestamps();
+        }
+        public function userMiembro()
+        {
+            return $this->belongsToMany('App\models\User', "equipo_user", "equipo_id", "user_id")
+                ->withPivot('user_id')->withTimestamps();
+        }
+        public function proyecto()
+        {
+            return $this->hasMany('App\models\Proyectos', 'equipos_id');
+        }
+        public function scopeMisequipos($query)
+        {
+            $usuario = Auth::user()->id;
+            return $query->whereHas('userMiembro', function ($q) use ($usuario) {
+                $q->where('user_id', $usuario);
+            })->get()->load("userMiembro");
+        }
+        
+        
+        
+        
     }
-
-
-
-}
